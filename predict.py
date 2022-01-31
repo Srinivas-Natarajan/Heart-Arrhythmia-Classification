ae5b31c8941b371eedc2b51cad0747be091519c6
unknown <nsrinivas06@gmail.com>
2022-01-31 20:08:56 +0530

    Simplified CNN model added as an option

--- predict.py
+++ predict.py
@@ -6,22 +6,27 @@ warnings.filterwarnings('ignore')
 
 from preprocessing import edftocsv
 
-def predict_class(edf_file_path, output_fname):
+def predict_class(edf_file_path, output_fname, model_no):
     """
         Input:
             edf_file_path - Path to the EDF file
             output_fname - Name of save file generated
+            model_no - Model to be used for prediction
         Output:
             labels - List containing labels of each segment between R-peaks
     """
-    #Load data and model
+
+    #Load data 
     df = edftocsv(edf_file_path, "ECG")
     classes = {0:'N', 1:'S', 2:'V', 3:'F', 4:'Q'} 
-    loaded_model = tf.keras.models.load_model('./my_model_30')
-
-    #Format loaded EDF file
-    X = df.iloc[:,:186].values
-    X = X.reshape(len(X), X.shape[1],1)
+    
+    if(model_no == 0):
+        loaded_model = tf.keras.models.load_model('./my_model_30')
+        X = df.iloc[:,:186].values
+        X = X.reshape(len(X), X.shape[1],1)
+    else:
+        loaded_model = tf.keras.models.load_model('./simpler_model')
+        X = df.iloc[:,:187].values
 
     #Predict values and save to the local system
     y_pred = loaded_model.predict(X, batch_size=1000)
@@ -29,6 +34,7 @@ def predict_class(edf_file_path, output_fname):
     labels = []
     for i in range(prediction.shape[0]):
         labels.append(classes[prediction[i]])
+
     np.savetxt('./files/' + output_fname + '.txt', labels, fmt='%s')
     np.save('./files/' + output_fname, labels)
 
@@ -38,8 +44,9 @@ def predict_class(edf_file_path, output_fname):
 try:
         edf_file_path = sys.argv[1]
         output_fname = sys.argv[2]
+        model_no = sys.argv[3]
 except IndexError:
         print("Invalid Argument structure")
 
-print("\n\nThe classes are: \n", predict_class(edf_file_path, output_fname))
+print("\n\nThe classes are: \n", predict_class(edf_file_path, output_fname, model_no))
 
